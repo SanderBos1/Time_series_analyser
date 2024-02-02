06623c0b2f754e532ed251ca0e8254ca73952693
Sander Bos <sanderb0ss@live.nl>
2024-02-02 15:48:46 +0100

    Removing files
    kept test files in code

--- dockerfile
+++ dockerfile
@@ -3,4 +3,5 @@ RUN apt update && \
     apt install -y python3-pip 
 COPY . .
 RUN pip install -r requirements.txt
+WORKDIR /ts_app
 CMD ["python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- ts_app/__init__.py
+++ ts_app/__init__.py
@@ -4,7 +4,7 @@ from ts_python import *
 from flask_session import Session
 
 app = Flask(__name__)
-app.config.from_object(config.Config)
+app.config.from_object(config.DevConfig)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
